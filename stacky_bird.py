d1b78b8b52ac590cff0237dbc32027a7765ef98c
cla114 <clayton.rossiter@gmail.com>
2020-06-19 10:59:22 +0100

    Added adaptable screen resolution for bird mapping

--- stacky_bird.py
+++ stacky_bird.py
@@ -1,6 +1,7 @@
 from ppadb.client import Client
 import time
 import mss
+import os
 
 class StackyBird():
     def __init__(self, host='127.0.0.1', port=5037):
@@ -14,15 +15,41 @@ class StackyBird():
         self.revivey = 1900
         self.nextLevelX = 545
         self.nextLevelY = 1945
+        self.colorBird = (253,251,125)
+        self.colorGrass = (0,247,160)
+        self.colorMudLight = (253,141,95)
+        self.colorMudDark = (238,123,78)
+        self.referenceGroundLevel= 500/800
+        self.referenceBlockHeight = 25/800
+        self.referenceBlockX = 75/1280
+        self.referenceScreenWidth = 260/1280
+        self.referenceScreenHeight = 500/800
+        self.referenceScreenBottom = 640/800
         self.stack = 125    # time in ms to add one stack
+        self.get_resolution()
         self.connect()
 
     def connect(self):
+        # connect to android device
         self.adb = Client(host=self.host, port=self.port)
         try:
             self.device = self.adb.devices()[0]
         except:
             print("Cannot connect to phone!")
+    
+    def get_resolution(self):
+        # needed to get screen resolution to scale heights
+        # take screenshot and get number of pixels
+        sct = mss.mss()
+        img = sct.grab(sct.monitors[1])
+        self.monitorWidth = img.size[0]
+        self.monitorHeight = img.size[1]
+        self.blockHeight = self.referenceBlockHeight*self.monitorHeight
+        self.groundLevel = self.referenceGroundLevel*self.monitorHeight
+        self.screenHeight = self.referenceScreenHeight*self.monitorHeight
+        self.screenBottom = self.referenceScreenBottom*self.monitorHeight
+        self.screenWidth = self.referenceScreenWidth*self.monitorWidth
+        self.birdX = self.referenceBlockX*self.monitorWidth
 
     def tap(self, x, y):
         command = 'input tap {} {}'.format(x,y)
@@ -49,13 +76,16 @@ class StackyBird():
         self.start()
 
     def screenshot(self):
-        self.screenshot = mss()
+        self.screenshot = mss.mss()
+        # get pixel matrix
+
+
+    def update(self):
+        current_time = time.time()
+        self.screenshot()
 
 
 if __name__ == '__main__':
     game = StackyBird()
-    game.revive()
-    game.hold(125)
-    game.hold(250)
-    game.hold(375)
-        
+    # game.start()
+    # game.update()
