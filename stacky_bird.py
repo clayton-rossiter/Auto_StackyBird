deec73cb2749f1acc236bf27f7f2b9b75c819745
cla114 <clayton.rossiter@gmail.com>
2020-06-19 16:07:49 +0100

    Added iterable screenshot of device screen

--- stacky_bird.py
+++ stacky_bird.py
@@ -2,6 +2,7 @@ from ppadb.client import Client
 import time
 import mss
 import os
+import numpy as np
 
 class StackyBird():
     def __init__(self, host='127.0.0.1', port=5037):
@@ -19,15 +20,21 @@ class StackyBird():
         self.colorGrass = (0,247,160)
         self.colorMudLight = (253,141,95)
         self.colorMudDark = (238,123,78)
-        self.referenceGroundLevel= 500/800
-        self.referenceBlockHeight = 25/800
-        self.referenceBlockX = 75/1280
-        self.referenceScreenWidth = 260/1280
-        self.referenceScreenHeight = 500/800
-        self.referenceScreenBottom = 640/800
         self.stack = 125    # time in ms to add one stack
+        self.refresh = 125  # time to move one block
+        # below parameters were tested on a 1280x800 monitor
+        # relative sizes are later calculated in get_resolution()
+        self.referenceGroundLevel= 500/800
+        self.referenceBlockSize = 25/800
+        self.referencePaddingLeft = 60/1280     # padding of bird from left screen
+        self.referenceScreenWidth = 260/1280    # width of playable device screen
+        self.referenceScreenHeight = 500/800    # height of playable device screen
+        self.referenceScreenTop = 145/800       # distance in px from 0,0 to playable screen top
         self.get_resolution()
         self.connect()
+
+        def initialise_grid():
+            # create empty grid of values for colours
 
     def connect(self):
         # connect to android device
@@ -40,16 +47,18 @@ class StackyBird():
     def get_resolution(self):
         # needed to get screen resolution to scale heights
         # take screenshot and get number of pixels
+        # currently only works with 1 monitor, or game working on first monitor
         sct = mss.mss()
         img = sct.grab(sct.monitors[1])
         self.monitorWidth = img.size[0]
         self.monitorHeight = img.size[1]
-        self.blockHeight = self.referenceBlockHeight*self.monitorHeight
+        self.blockSize = self.referenceBlockSize*self.monitorHeight
         self.groundLevel = self.referenceGroundLevel*self.monitorHeight
         self.screenHeight = self.referenceScreenHeight*self.monitorHeight
-        self.screenBottom = self.referenceScreenBottom*self.monitorHeight
+        self.screenTop= self.referenceScreenTop*self.monitorHeight
+        self.paddingLeft = self.referencePaddingLeft*self.monitorWidth
         self.screenWidth = self.referenceScreenWidth*self.monitorWidth
-        self.birdX = self.referenceBlockX*self.monitorWidth
+        # self.birdX = self.referenceBlockX*self.monitorWidth
 
     def tap(self, x, y):
         command = 'input tap {} {}'.format(x,y)
@@ -75,17 +84,29 @@ class StackyBird():
         print('Level complete!')
         self.start()
 
-    def screenshot(self):
-        self.screenshot = mss.mss()
-        # get pixel matrix
-
-
     def update(self):
-        current_time = time.time()
-        self.screenshot()
+        while True:
+            current_time = time.time()
+            self.check_grid()
+            time.sleep(time.time()-current_time)
+
+        def check_grid(self):
+            # approximately 10 blocks per screen width
+            # approximately 20 blocks per screen height
+            # only need to consider blocks right of bird (3 blocks right)
+            # monitor centre point of grid 
+            self.screenshot()
+
+        
+        def screenshot(self):
+            # take screenshot of just the phone screen
+            # assuming Apowermirror screen is far left of screen
+            with mss.mss() as sct:
+                monitor = {"top": self.screenTop, "left": 0, "width": self.screenWidth, "height": self.screenHeight}
+                self.rgba = np.array(sct.grab(monitor))
+            # count current height of bird
+            
 
 
 if __name__ == '__main__':
     game = StackyBird()
-    # game.start()
-    # game.update()
